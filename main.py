6ac2af443b745d388ad3df2b1e7e423f65b1030b
dcode-otb <frontend@agenciaotb.com>
2022-03-01 10:24:58 -0600

    validation_path

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Dict, Optional
 # Pydantic
 from pydantic import BaseModel
 # FastApi
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Body, Path, Query
 
 #  Instancia de la clase.
 app: FastAPI = FastAPI()
@@ -31,7 +31,30 @@ def create_person(person: Person = Body(...)):
 # Validaciones query parameters
 @app.get('/person/detail')
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: str = Query(...),
+    name: Optional[str] = Query(
+        None,
+        min_length=1,
+        max_length=50,
+        title='Person name',
+        description='This is the person name. It\'s between 1 and 50 characters'
+    ),
+    age: str = Query(
+        ...,
+        title='Person age',
+        description='This is the person name. It\'s required'
+    ),
 ):
     return {name: age}
+
+
+# Validaciones: path parameters
+@app.get('/person/detail/{person_id}')
+def show_person(
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title='Person id',
+        description='This is the person id. It\'s required and value is greater than 0'
+    )
+):
+    return {person_id: 'it exists'}
